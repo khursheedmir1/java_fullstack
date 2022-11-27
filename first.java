8d4b475a2158fed395e1809d8c6c027d53a56da8
misbah uddin <misbahu1994@gmail.com>
2022-11-27 11:00:32 -0500

    new update

--- first.java
+++ first.java
@@ -3,5 +3,14 @@ class a
 public static void main(String[] args) {
 	System.out.print("helloworld,");
 	System.out.print("From USA");
+	
+	//   new DataType
+
+	int i = 5; /*this datatype is integer*/
+	System.out.println("this i represent number of people are programmer:"+ i);
+	float j =7.255F; /*this datatype is float*/
+	System.out.println("PRINT THIS FLOAT NUMBER:"+ j);
+	char ThisIsToTestTheChar ='G';/*this datatype is char*/
+	System.out.println("char testing:"+ ThisIsToTestTheChar);
 }
 }
